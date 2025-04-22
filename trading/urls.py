f0361d3612caaf14f92cdc000503659c349c3d9b
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2025-04-22 00:45:45 -0400

    bugfix

--- trading/urls.py
+++ trading/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
         path("accounts/profile/<int:pk>/collection",
              v.UserPokemonListView.as_view(), name="user_collection"),
         path("accounts/profile/<int:pk>/wishlist",
-             v.UserPokemonWishListView.as_view(), name="user_collection"),
+             v.UserPokemonWishListView.as_view(), name="user_wl"),
         path("pokemon/<int:pk>/wish", v.WishPokemonView.as_view(),
              name="wish"),
         path("pokemon/<int:pk>/unwish", v.UnWishPokemonView.as_view(),
